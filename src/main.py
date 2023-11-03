50b67014728fcc49abbdfc02f18cac8368a0cfa8
diana <diana-safina-2017@mail.ru>
2023-11-03 21:16:28 +0500

    venv

--- src/main.py
+++ src/main.py
@@ -8,17 +8,19 @@ while True:
     while True:
         if int(choice_user) == 1:
             vac_hh = create_user_vacanci_Hh()
-            if vac_hh != []:
+            if vac_hh:
                 user_JsonSave = create_user_JsonSave(vac_hh)
                 user_info = choose_prof(user_JsonSave)
+                break
             else:
                 break
 
         elif int(choice_user) == 2:
             vac_sj = create_user_vacanci_Sj()
-            if vac_sj != []:
+            if vac_sj:
                 user_JsonSave = create_user_JsonSave_SJ(vac_sj)
                 user_info = choose_prof(user_JsonSave)
+                break
             else:
                 break
 
@@ -27,11 +29,12 @@ while True:
             vac_hh = create_user_vacanci_Hh()
             print("Данные для Sj")
             vac_sj = create_user_vacanci_Sj()
-            if vac_sj != [] and vac_hh != []:
+            if vac_sj and vac_hh:
                 user_JsonSave1 = create_user_JsonSave(vac_hh)
                 user_JsonSave2 = create_user_JsonSave_SJ(vac_sj)
                 all_JsonSave = user_JsonSave1 + user_JsonSave2
                 user_info = choose_prof(all_JsonSave)
+                break
             else:
                 break
         elif int(choice_user) == 4:
